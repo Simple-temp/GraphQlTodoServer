eae8421f0120365ed8cc27363a32ebd934ac115b
Simple-temp <fiveriam01@gmail..com>
2022-06-04 14:25:58 +0600

    make it better

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ import dotenv from "dotenv";
 import express from 'express';
 import http from 'http';
 import path from "path"
+const __dirname = path.resolve()
 
 const port = process.env.PORT || 4000
 const app = express();
