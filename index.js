d07ca0a4c9f690f5a5ee4b4134d2ad993aa33909
Simple-temp <fiveriam01@gmail..com>
2022-06-04 17:52:19 +0600

    make it better

--- index.js
+++ index.js
@@ -62,10 +62,12 @@ const server = new ApolloServer({
 })
 
 if (process.env.NODE_ENV == "production") {
+
     app.use(express.static("../merng-client/build"))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "merng-client", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "../merng-client/build/index.html"))
     })
+
 }
 
 await server.start();
